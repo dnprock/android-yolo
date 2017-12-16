9e917dc02075938f056760549f04526ab3d51bc8
Phuoc Do <dnprock@gmail.com>
2017-12-15 18:19:35 -0800

    Login activity

--- src/co/vidalab/yolo/LoginActivity.java
+++ src/co/vidalab/yolo/LoginActivity.java
@@ -1,13 +1,67 @@
 package co.vidalab.yolo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends Activity {
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.Task;
+
+public class LoginActivity extends Activity implements View.OnClickListener {
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 007;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        SignInButton loginButton = (SignInButton) findViewById(R.id.sign_in_button);
+        loginButton.setOnClickListener(this);
+    }
+
+    public void onClick(View arg0) {
+        // Start DetectorActivity.class
+        //Intent detectorActivity = new Intent(LoginActivity.this,
+        //        DetectorActivity.class);
+        //startActivity(detectorActivity);
+
+        signIn();
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            //Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            //handleSignInResult(task);
+
+            // Start DetectorActivity.class
+            Intent detectorActivity = new Intent(LoginActivity.this,
+                    DetectorActivity.class);
+            startActivity(detectorActivity);
+        }
     }
 }
